9e34179d4df3bce4fd1423c4e68b468a00f7d157
Haco <haco.tw@gmail.com>
2026-04-21 12:55:44 +0800

    feat: add WebSocket dependency and 3-player dealing support
    - Add java-websocket library and maven-shade-plugin to pom.xml
    - Add Deck.dealToPlayers3() for 3-player mode: 52 cards fully dealt,
      Club 3 holder receives 18 cards, others get 17
    - Add --server flag to Main.java to launch headless server mode

--- Big2/src/main/java/com/bigtwo/Main.java
+++ Big2/src/main/java/com/bigtwo/Main.java
@@ -1,12 +1,19 @@
 package com.bigtwo;
 
+import com.bigtwo.server.GameServer;
 import com.bigtwo.ui.GameWindow;
 
 import javax.swing.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // Use system look and feel
+        if (args.length > 0 && args[0].equals("--server")) {
+            String[] serverArgs = args.length > 1 ? Arrays.copyOfRange(args, 1, args.length) : new String[0];
+            GameServer.main(serverArgs);
+            return;
+        }
+
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception ignored) {}
--- Big2/src/main/java/com/bigtwo/model/Deck.java
+++ Big2/src/main/java/com/bigtwo/model/Deck.java
@@ -34,4 +34,40 @@ public class Deck {
         for (List<Card> hand : hands) Collections.sort(hand);
         return hands;
     }
+
+    /**
+     * 3-player deal: all 52 cards dealt, player with Club 3 gets 18 cards, others get 17.
+     */
+    public List<List<Card>> dealToPlayers3() {
+        shuffle();
+        List<List<Card>> hands = new ArrayList<>();
+        for (int i = 0; i < 3; i++) hands.add(new ArrayList<>());
+        // Deal 51 cards evenly (17 each)
+        for (int i = 0; i < 51; i++) {
+            hands.get(i % 3).add(cards.get(i));
+        }
+        // Find who has Club 3
+        Card lastCard = cards.get(51);
+        int clubThreeOwner = -1;
+        for (int i = 0; i < 3; i++) {
+            for (Card c : hands.get(i)) {
+                if (c.getRank() == Rank.THREE && c.getSuit() == Suit.CLUBS) {
+                    clubThreeOwner = i;
+                    break;
+                }
+            }
+            if (clubThreeOwner >= 0) break;
+        }
+        // The last card might itself be Club 3
+        if (clubThreeOwner < 0 && lastCard.getRank() == Rank.THREE && lastCard.getSuit() == Suit.CLUBS) {
+            // Give to player 0 (they'll have Club 3)
+            hands.get(0).add(lastCard);
+        } else if (clubThreeOwner >= 0) {
+            hands.get(clubThreeOwner).add(lastCard);
+        } else {
+            hands.get(0).add(lastCard);
+        }
+        for (List<Card> hand : hands) Collections.sort(hand);
+        return hands;
+    }
 }
